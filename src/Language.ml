263a14899661554f3c0bcb61d306e07f8aafa6af
SamokhinWorking <samokhin.working@gmail.com>
2019-03-28 01:20:35 +0300

    hw 5

--- src/Language.ml
+++ src/Language.ml
@@ -36,15 +36,47 @@ module Expr =
       to value v and returns the new state.
     *)
     let update x v s = fun y -> if x = y then v else s y
+                                                      
+    (*Convert int to bool*)
+    let intToBool var = var != 0
+    (*Convert bool to int*)
+    let boolToInt var = if var then 1 else 0 
+    (* int->int-> bool to int -> int -> int *)
+    let compareInt f var1 var2 = boolToInt ( f var1 var2 )
+    (*bool -> bool_-> bool to int-> int-> int *)
+    let compareBool f var1 var2 = boolToInt (f (intToBool var1) (intToBool var2 ) )
+    (*Convert operator to Ocaml*)
+    let evalOperation op = match op with
+      | "+"  -> ( + )
+      | "-"  -> ( - )
+      | "*"  -> ( * )
+      | "/"  -> ( / )
+      | "%"  -> ( mod ) 
+      | "<"  -> compareInt( < )
+      | ">"  -> compareInt( > )
+      | "<=" -> compareInt( <= )
+      | ">=" -> compareInt( >= )
+      | "==" -> compareInt( = )
+      | "!=" -> compareInt( <> )
+      | "&&" -> compareBool( && )
+      | "!!" -> compareBool( || )
+      | _    -> failwith "Unknown operator" ;;
+
 
     (* Expression evaluator
 
-          val eval : state -> t -> int
- 
+         val eval : state -> expr -> int
+     
        Takes a state and an expression, and returns the value of the expression in 
        the given state.
-    *)                                                       
-    let eval st expr = failwith "Not yet implemented"
+    *)
+
+    let rec eval state expr = match expr with
+      | Const var -> var 
+      | Var str -> state str
+      | Binop (op, expr1, expr2) -> evalOperation op (eval state expr1) (eval state expr2) 
+
+  let parseBinop op = ostap(- $(op)),(fun expr1 expr2 -> Binop (op,expr1,expr2))
 
     (* Expression parser. You can use the following terminals:
 
@@ -52,11 +84,25 @@ module Expr =
          DECIMAL --- a decimal constant [0-9]+ as a string
                                                                                                                   
     *)
-    ostap (                                      
-      parse: empty {failwith "Not yet implemented"}
-    )
-    
-  end
+   ostap (
+        expr:
+          !(Ostap.Util.expr
+            (fun x-> x)
+            (Array.map (fun (assoc,op) -> assoc,List.map parseBinop op)
+              [|
+                `Lefta, ["!!"];
+                `Lefta, ["&&"];
+                `Nona,  ["<=";"<";">=";">";"!=";"==";];
+                `Lefta, ["+";"-"];
+                `Lefta, ["*";"/";"%"];
+              |]
+            )
+            primary
+          );
+        primary: x:IDENT {Var x}| c:DECIMAL {Const c}| -"(" expr -")"
+      )
+
+    end
                     
 (* Simple statements: syntax and sematics *)
 module Stmt =
@@ -71,26 +117,54 @@ module Stmt =
     (* empty statement                  *) | Skip
     (* conditional                      *) | If     of Expr.t * t * t
     (* loop with a pre-condition        *) | While  of Expr.t * t
-    (* loop with a post-condition       *) (* add yourself *)  with show
+    (* loop with a post-condition       *) | Repeat of Expr.t * t  with show
                                                                     
     (* The type of configuration: a state, an input stream, an output stream *)
     type config = Expr.state * int list * int list 
-
     (* Statement evaluator
 
          val eval : config -> t -> config
 
        Takes a configuration and a statement, and returns another configuration
     *)
-    let rec eval conf stmt = failwith "Not yet implemented"
-                               
+    let rec eval (state,input,output) stmt = match stmt with
+      | Read x -> (match input with
+        | z::tail ->(Expr.update x z state, tail, output)
+        | _ -> failwith "Read")
+      | Write e -> (state,input,output @ [Expr.eval state e])
+      | Assign (x,e)-> (Expr.update x (Expr.eval state e) state, input,output)
+      | Seq (e1,e2) -> eval (eval (state,input,output) e1) e2
+      | Skip -> (state,input,output)
+      | If (e,s1,s2) -> if Expr.eval state e != 0 then eval (state,input,output) s1 else eval (state,input,output) s2
+      | While (e,s) -> if Expr.eval state e != 0 then eval (eval (state,input,output) s) stmt else (state,input,output)
+      | Repeat (e,s) -> let (state_, input_, output_) as conf_ = eval (state,input,output) s in
+                        if Expr.eval state_ e ==0 then eval conf_ stmt else conf_
+
+
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not yet implemented"}
+      parse: 
+      line:stmt ";" rest:parse {Seq (line,rest)} 
+      | stmt;
+      stmt: read | write | assign | skip | if' | while' | for'| repeat;
+      read: "read"  "("  x:IDENT ")"          { Read x };
+      write: "write" "("  exp:!(Expr.expr) ")" { Write exp };
+      assign: x:IDENT ":=" exp:!(Expr.expr)     { Assign (x,exp) };
+      skip: "skip" {Skip};
+      if': "if" exp:!(Expr.expr) "then" s1:parse s2:else_s {If (exp,s1,s2) };
+
+      else_s: 
+      "fi" {Skip}
+      | "else" s:parse "fi" {s}
+      | "elif" exp:!(Expr.expr) "then" s1:parse s2:else_s {If(exp,s1,s2)}
+      ;
+
+      while':"while" e:!(Expr.expr) "do" s:parse "od" {While (e,s)};
+      for': "for" s1:parse "," e:!(Expr.expr) "," s2:parse "do" s3:parse "od" {Seq(s1,While (e,Seq(s3,s2)))};
+      repeat: "repeat" s:parse "until" e:!(Expr.expr) {Repeat (e,s)}
     )
       
   end
-
 (* The top-level definitions *)
 
 (* The top-level syntax category is statement *)
--- src/SM.ml
+++ src/SM.ml
@@ -1,5 +1,6 @@
 open GT       
 open Language
+open List
        
 (* The type for the stack machine instructions *)
 @type insn =
@@ -21,21 +22,63 @@ type prg = insn list
  *)
 type config = int list * Stmt.config
 
+
 (* Stack machine interpreter
 
-     val eval : env -> config -> prg -> config
+     val eval : config -> prg -> config
 
-   Takes an environment, a configuration and a program, and returns a configuration as a result. The
-   environment is used to locate a label to jump to (via method env#labeled <label_name>)
-*)                         
-let rec eval env conf prog = failwith "Not yet implemented"
+   Takes a configuration and a program, and returns a configuration as a result
+*)  
 
+let rec eval env config programm  =
+  let (stack, stmtConfig) = config in
+  let (state, input, output) = stmtConfig in
+  match programm with
+  | []            -> config
+  | instr :: rest -> 
+    let evalCode config = eval env config rest in
+    let evalJmp config label = eval env config (env#labeled label) in
+    match instr with
+      | BINOP op   -> evalCode
+        (match stack with
+        | y :: x :: tail -> ((Expr.evalOperation op x y) :: tail, stmtConfig)
+        | _              -> failwith "BINOP"
+        )
+      | CONST n    -> evalCode (n :: stack, stmtConfig)
+      | READ       -> evalCode
+        (match input with
+        | head :: tail -> (head :: stack, (state, tail, output))
+        | _            -> failwith "READ"
+        )
+      | WRITE       -> evalCode
+        (match stack with
+        | head :: tail -> (tail, (state, input, output @ [head]))
+        | _            -> failwith "WRITE"
+        )
+      | LD x        -> evalCode (state x :: stack, stmtConfig)
+      | ST x        -> evalCode
+        (match stack with
+        | head :: tail -> (tail, (Expr.update x head state, input, output))
+        | _            -> failwith "ST"
+        )
+      | LABEL _     -> evalCode config
+      | JMP l       -> evalJmp config l 
+      | CJMP (c, l) ->
+        (match stack with
+        | head :: tail -> if c = "z" && head = 0 || c = "nz" && head != 0
+                          then evalJmp (tail, stmtConfig) l
+                          else evalCode config
+        | _            -> failwith "CJMP"
+)
+    
 (* Top-level evaluation
 
      val run : prg -> int list -> int list
 
    Takes a program, an input stream, and returns an output stream this program calculates
 *)
+
+
 let run p i =
   let module M = Map.Make (String) in
   let rec make_map m = function
@@ -46,6 +89,18 @@ let run p i =
   let m = make_map M.empty p in
   let (_, (_, _, o)) = eval (object method labeled l = M.find l m end) ([], (Expr.empty, i, [])) p in o
 
+
+
+let compEnv = 
+  object (self)
+    val mutable label_number = 0
+
+  method generateLabel =
+  label_number <- label_number +1;
+  ".label"^(string_of_int label_number)
+
+  end
+
 (* Stack machine compiler
 
      val compile : Language.Stmt.t -> prg
@@ -53,4 +108,51 @@ let run p i =
    Takes a program in the source language and returns an equivalent program for the
    stack machine
 *)
-let compile p = failwith "Not yet implemented"
+
+let rec compileLabel p lastLabel =
+  let rec expr = function
+  | Expr.Var   x          -> [LD x]
+  | Expr.Const n          -> [CONST n]
+  | Expr.Binop (op, x, y) -> expr x @ expr y @ [BINOP op]
+  in match p with
+  | Stmt.Seq (s1, s2)  ->
+    let newLabel = compEnv#generateLabel  in
+    let (compiled1, used1) = compileLabel s1 newLabel in
+    let (compiled2, used2) = compileLabel s2 lastLabel in
+    compiled1 @ (if used1 then [LABEL newLabel] else []) @ compiled2, used2
+  | Stmt.Read x        -> [READ; ST x], false
+  | Stmt.Write e       -> expr e @ [WRITE], false
+  | Stmt.Assign (x, e) -> expr e @ [ST x], false
+  | Stmt.Skip          -> [], false
+  | Stmt.If (e, s1, s2)  ->
+    let l_else = compEnv#generateLabel  in
+    let (if_body, used1) = compileLabel s1 lastLabel in
+    let (else_body, used2) = compileLabel s2 lastLabel in
+    expr e @ [CJMP ("z", l_else)] @ if_body @ (if used1 then [] else [JMP lastLabel]) @ [LABEL l_else] @ else_body @ (if used2 then [] else [JMP lastLabel]), true
+  | Stmt.While (e, s)  ->
+    let check = compEnv#generateLabel  in
+    let loop = compEnv#generateLabel in
+    let (body, _) = compileLabel s check in
+    [JMP check; LABEL loop] @ body @ [LABEL check] @ expr e @ [CJMP ("nz", loop)], false
+  | Stmt.Repeat (e, s) ->
+      let l_repeat = compEnv#generateLabel  in
+      let (body, _) = compileLabel s lastLabel in
+      [LABEL l_repeat] @ body @ expr e @ [CJMP ("z", l_repeat)], false
+
+let rec compile p =
+    let l = compEnv#generateLabel  in
+    let compiled, used = compileLabel p l in
+compiled @ (if used then [LABEL l] else [])
+
+
+
+(* Stack machine compiler
+
+     val compile : Language.Stmt.t -> prg
+
+   Takes a program in the source language and returns an equivalent program for the
+   stack machine
+*)
+
+
+
