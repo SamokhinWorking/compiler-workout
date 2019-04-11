2686ef8423439882e7b5ddfcc825a24f6d8849e6
SamokhinWorking <samokhin.working@gmail.com>
2019-04-11 16:21:40 +0300

    hw7 fix

--- src/X86.ml
+++ src/X86.ml
@@ -182,7 +182,7 @@ let rec compile env prg  = match prg with
 module S = Set.Make (String)
 
 (* Environment implementation *)
-let make_assoc l = List.combine l (List.init (List.length l) (fun x -> x))
+let make_assoc l = List.combine l (list_init (List.length l) (fun x -> x))
                      
 class env =
   object (self)
